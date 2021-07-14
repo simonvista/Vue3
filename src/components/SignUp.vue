8bf568084828ccfc87325f67bae607a2051d1129
YL <simon-ling@hotmail.com>
2021-07-14 09:23:14 -0400

    4-0000 is done

---
+++ src/components/SignUp.vue
@@ -0,0 +1,40 @@
+<template>
+  <img class="logo" src="../assets/resto-logo.jpg" alt="" />
+  <h1>Sign Up</h1>
+  <div class="register">
+    <input type="text" placeholder="name" />
+    <input type="email" placeholder="email" />
+    <input type="password" placeholder="password" />
+    <button>sign up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+
+<style>
+.logo {
+  width: 100px;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding: 20px;
+  display: block;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+}
+.register button {
+  width: 320px;
+  border: 1px;
+  border: 1px solid skyblue;
+  background: skyblue;
+  color: white;
+  cursor: pointer;
+}
+</style>
