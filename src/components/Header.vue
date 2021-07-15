09e907d2718a5dcebe9c7424a15d77de442f55b3
YL <simon-ling@hotmail.com>
2021-07-14 22:48:11 -0400

    11-0000 is done

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="nav">
+    <a href="#">Home</a>
+    <a href="#">Add Restaurant</a>
+    <a href="#">Upadte Restaurant</a>
+    <a href="#">Logout</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+.nav {
+  background-color: #333;
+  overflow: hidden;
+}
+.nav a {
+  float: left;
+  color: white;
+  padding: 14px 16px;
+  text-align: center;
+  font-size: 17px;
+  text-decoration: none;
+  margin-left: 5px;
+}
+.nav a:hover {
+  background: #ddd;
+  color: #333;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,11 @@
 <template>
+  <Header />
   <h1>home page</h1>
 </template>
 <script>
+import Header from "./Header.vue";
 export default {
+  components: { Header },
   name: "Home",
   mounted() {
     let user = localStorage.getItem("user-info");
