4368161f0c70b5e60f76d6038701bc4b921ec5fa
YL <simon-ling@hotmail.com>
2021-07-14 20:15:48 -0400

    8-0000 is done

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -40,6 +40,12 @@ export default {
       }
     },
   },
+  mounted() {
+    let user = localStorage.getItem("user-info");
+    if (user) {
+      this.$router.push({ name: "Home" });
+    }
+  },
 };
 </script>
 
