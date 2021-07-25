edd6d675a248d81d246d730faccdb4c17fa8ecef
YL <simon-ling@hotmail.com>
2021-07-25 09:54:19 -0400

    12-0000 is done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,25 @@
 <template>
   <h1>
-    Get input values through 2-way binding (bind value from view and model
-    sides)
+    Get checkbox & radio button values through 2-way binding (bind value from
+    view and model sides)
   </h1>
-  <input type="email" placeholder="email" v-model="email" /><br /><br />
-  <input
-    type="password"
-    placeholder="password"
-    v-model="password"
-  /><br /><br />
-  <button type="button" v-on:click="getInputs">getInputs</button>
+  <h1>Technologies</h1>
+  <input type="checkbox" value="php" id="php" v-model="technologies" />
+  <label for="php">php</label>
+  <input type="checkbox" value="java" id="java" v-model="technologies" />
+  <label for="java">java</label>
+  <input type="checkbox" value="python" id="python" v-model="technologies" />
+  <label for="python">python</label>
+  <!-- <h4>Technology Info: {{ technologies }}</h4> -->
+  <br /><br />
+  <h1>Profession</h1>
+  <label for="student">student</label>
+  <input type="radio" value="student" id="student" v-model="profession" />
+  <label for="teacher">teacher</label>
+  <input type="radio" value="teacher" id="teacher" v-model="profession" />
+  <label for="pupil">pupil</label>
+  <input type="radio" value="pupil" id="pupil" v-model="profession" />
+  <h4>Technology Info: {{ technologies }}, I'm a {{ profession }}</h4>
 </template>
 
 <script>
@@ -17,14 +27,12 @@ export default {
   name: "Home",
   data() {
     return {
-      email: null,
-      password: null,
+      technologies: [],
+      profession: null,
     };
   },
   methods: {
-    getInputs() {
-      console.warn(`${this.email}, ${this.password}`);
-    },
+    getInputs() {},
   },
 };
 </script>
