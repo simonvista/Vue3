49ee4349e624bd69b8e4e4592ecbb87498986a41
YL <simon-ling@hotmail.com>
2021-07-24 23:48:41 -0400

    7-0000 is done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,24 @@
 <template>
   <h1>Home Component</h1>
+  <!-- interpolation -->
+  <h1>{{ 3 / 2 }}</h1>
+  <h1>{{ "hello".length }}</h1>
+  <h1>{{ email }}, {{ mobile }}</h1>
+  <h1>{{ getName("anil sidhu") }}</h1>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      email: "anil@test.com",
+      mobile: 12345,
+      getName(val) {
+        return val;
+      },
+    };
+  },
 };
 </script>
 
