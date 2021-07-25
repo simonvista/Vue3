9711b6191a2e592aada04b423ee47a24b15ea06d
YL <simon-ling@hotmail.com>
2021-07-24 23:38:45 -0400

    6-0000 is done

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Home Component</h1>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+// scoped make css local
+<style scoped>
+h1 {
+  color: orange;
+}
+</style>
