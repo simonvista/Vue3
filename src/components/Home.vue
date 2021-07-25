bab1dd3f2bbbd2e47e65346375cf9e15ee5215f1
YL <simon-ling@hotmail.com>
2021-07-25 18:48:05 -0400

    18-0000 is done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,27 @@
 <template>
-  <h1>Html Binding (v-html)</h1>
-  <div v-html="tag1"></div>
-  <div v-html="tag2"></div>
+  <h1>Class Binding (:class="{class1:true,class2:false,class3:true...}")</h1>
+  <h2 class="green">Home Component 1</h2>
   <hr />
-  <div v-html="tag1 + tag2 + tag3"></div>
+  <h2 :class="{ green: true }">Home Component 2</h2>
+  <h2 :class="{ green: isGreen }">Home Component 3</h2>
+  <hr />
+
+  <h2 :class="{ green: false }">Home Component 4</h2>
+  <h2 :class="{ green: !isGreen }">Home Component 5</h2>
+  <hr />
+  <button type="button" v-on:click="isGreen = !isGreen">
+    toggle green
+  </button>
+  <hr />
+  <h2 class="staticClass" :class="{ green: true }">
+    static class & single class binding
+  </h2>
+  <h2 class="staticClass" :class="{ green: true, err: true }">
+    static class & multiple classes binding
+  </h2>
+  <h2 class="staticClass" :class="adjustedStyle">
+    static class & multiple classes binding w/ computed
+  </h2>
 </template>
 
 <script>
@@ -12,11 +30,17 @@ export default {
   name: "Home",
   data() {
     return {
-      text: "anil sidhu",
-      tag1: "<h2>Anil Sidhu</h2>",
-      tag2: "<h2>Peter</h2>",
-      tag3: "<h2>Bruce</h2>",
+      isGreen: true,
     };
+  },
+  computed: {
+    adjustedStyle() {
+      return {
+        green: this.isGreen,
+        err: true,
+        other: false,
+      };
+    },
   },
   methods: {},
 };
@@ -26,5 +50,20 @@ export default {
 <style scoped>
 h1 {
   color: orange;
+}
+.green {
+  background-color: green;
+  width: 200px;
+  padding: 10px;
+  color: #fff;
+}
+.staticClass {
+  color: blue;
+}
+.err {
+  color: red;
+}
+.other {
+  font-size: 70px;
 }
 </style>
