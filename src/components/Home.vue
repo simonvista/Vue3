b12459bc656e5bb5c07d630f1bd90e8590301393
YL <simon-ling@hotmail.com>
2021-07-25 00:05:48 -0400

    8-0000 is done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  <h1>Home Component</h1>
+  <h1>Method w/ parameters</h1>
   <!-- interpolation -->
-  <h1>{{ 3 / 2 }}</h1>
-  <h1>{{ "hello".length }}</h1>
   <h1>{{ email }}, {{ mobile }}</h1>
-  <h1>{{ getName("anil sidhu") }}</h1>
+  <h1>{{ getName("Anil Sidhu") }}</h1>
+  <h1>{{ getData() }}</h1>
+  <h1>{{ getData().name }}, {{ getData().email }}</h1>
 </template>
 
 <script>
@@ -13,11 +13,18 @@ export default {
   data() {
     return {
       email: "anil@test.com",
-      mobile: 12345,
-      getName(val) {
-        return val;
-      },
     };
+  },
+  methods: {
+    getName(val) {
+      return val;
+    },
+    getData() {
+      return {
+        name: "anil",
+        email: this.email,
+      };
+    },
   },
 };
 </script>
