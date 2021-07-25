ff22c0d5b78c330d802c89cee4d8a35aa2f39f8c
YL <simon-ling@hotmail.com>
2021-07-25 10:04:28 -0400

    13-0000 is done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,8 @@
 <template>
-  <h1>
-    Get checkbox & radio button values through 2-way binding (bind value from
-    view and model sides)
-  </h1>
-  <h1>Technologies</h1>
-  <input type="checkbox" value="php" id="php" v-model="technologies" />
-  <label for="php">php</label>
-  <input type="checkbox" value="java" id="java" v-model="technologies" />
-  <label for="java">java</label>
-  <input type="checkbox" value="python" id="python" v-model="technologies" />
-  <label for="python">python</label>
-  <!-- <h4>Technology Info: {{ technologies }}</h4> -->
-  <br /><br />
-  <h1>Profession</h1>
-  <label for="student">student</label>
-  <input type="radio" value="student" id="student" v-model="profession" />
-  <label for="teacher">teacher</label>
-  <input type="radio" value="teacher" id="teacher" v-model="profession" />
-  <label for="pupil">pupil</label>
-  <input type="radio" value="pupil" id="pupil" v-model="profession" />
-  <h4>Technology Info: {{ technologies }}, I'm a {{ profession }}</h4>
+  <h1>Conditon</h1>
+  <h4 v-if="show">Show is true</h4>
+  <h4 v-else>Show is off</h4>
+  <button v-on:click="show = !show">toggle show</button>
 </template>
 
 <script>
@@ -27,13 +10,10 @@ export default {
   name: "Home",
   data() {
     return {
-      technologies: [],
-      profession: null,
+      show: false,
     };
   },
-  methods: {
-    getInputs() {},
-  },
+  methods: {},
 };
 </script>
 
