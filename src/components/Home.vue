0b59c651b6b16b4ea141b5e6b483325688127844
YL <simon-ling@hotmail.com>
2021-07-26 13:26:41 -0400

    31-0140 is done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,9 @@
 <template>
   <h1>Dyanamic components</h1>
-  <button v-on:click="loadJava">loadJava</button>
-  <button v-on:click="loadNode">loadNode</button>
-  <button v-on:click="loadPhp">loadPhp</button>
-  <Java v-if="load_Java" />
-  <Node v-if="load_Node" />
-  <Php v-if="load_Php" />
+  <button @click="tab = 'Java'">loadJava</button>
+  <button @click="tab = 'Node'">loadNode</button>
+  <button @click="tab = 'Php'">loadPhp</button>
+  <component :is="tab" />
 </template>
 
 <script>
@@ -22,29 +20,11 @@ export default {
   name: "Home",
   data() {
     return {
-      load_Java: false,
-      load_Node: false,
-      load_Php: false,
+      tab: "Java",
     };
   },
   computed: {},
-  methods: {
-    loadJava() {
-      this.load_Java = true;
-      this.load_Node = false;
-      this.load_Php = false;
-    },
-    loadNode() {
-      this.load_Java = false;
-      this.load_Node = true;
-      this.load_Php = false;
-    },
-    loadPhp() {
-      this.load_Java = false;
-      this.load_Node = false;
-      this.load_Php = true;
-    },
-  },
+  methods: {},
 };
 </script>
 
