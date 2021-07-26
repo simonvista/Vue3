b1859409b33b8503aa813011a1a96da820358e1e
YL <simon-ling@hotmail.com>
2021-07-26 10:46:43 -0400

    26-0650 is done

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,13 +1,16 @@
 <template>
-  <!-- Non-Props data -->
-  <h1 >user component</h1>
-  <!-- values of item & name still pass through h3 tag even no settings in Props -->
-  <!-- <h3 v-bind='$attrs'>Bruce</h3> -->
-  <h3 :='$attrs'>Bruce</h3>
-  <h3>Peter</h3>
-  <h3>Tom</h3>
-  <!-- Props data -->
-  <!-- <h3>{{ name }}, {{ item }}</h3> -->
+  <!-- values of item & name still pass through div tag even no settings in Props -->
+  <div>
+    <!-- Non-Props data -->
+    <h1 >user component</h1>
+    <!-- values of item & name still pass through h3 tag even no settings in Props -->
+    <!-- <h3 v-bind='$attrs'>Bruce</h3> -->
+    <h3 :='$attrs'>Bruce</h3>
+    <h3>Peter</h3>
+    <h3>Tom</h3>
+    <!-- Props data -->
+    <!-- <h3>{{ name }}, {{ item }}</h3> -->    
+  </div>
 </template>
 
 <script>
