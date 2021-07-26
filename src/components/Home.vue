c837965d10c5e39e85646c68ebac3b7dac754470
YL <simon-ling@hotmail.com>
2021-07-26 10:16:05 -0400

    26-0000 is done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,92 +1,21 @@
 <template>
-  <h1>simple form</h1>
-  <!-- {{ form }} -->
-  <ul>
-    <li v-for="err in errs" :key="err.id" style="color:red">
-      {{ err }} is not valid
-    </li>
-  </ul>
-  <form @submit="login">
-    <!-- input field -->
-    <label>email</label>
-    <input type="email" placeholder="email" v-model="form.email" /><br /><br />
-    <label>password</label>
-    <input
-      type="password"
-      placeholder="password"
-      v-model="form.password"
-    /><br /><br />
-
-    <!-- select box -->
-    <h1>country</h1>
-    <select v-model="form.country">
-      <option value="india">india</option>
-      <option value="usa">usa</option>
-    </select>
-    <br /><br />
-
-    <!-- radio box -->
-    <h1>gender</h1>
-    <label>male</label>
-    <input
-      type="radio"
-      name="gender"
-      value="male"
-      v-model="form.gender"
-    /><br /><br />
-    <label>female</label>
-    <input
-      type="radio"
-      name="gender"
-      value="female"
-      v-model="form.gender"
-    /><br /><br />
-
-    <!-- checkbox -->
-    <h1>tech</h1>
-    <label>java</label>
-    <input type="checkbox" value="java" v-model="form.tech" /><br /><br />
-    <label>python</label>
-    <input type="checkbox" value="python" v-model="form.tech" /><br /><br />
-
-    <button type="submit">submit</button>
-  </form>
+  <h1>Non-Props Data</h1>
+  <user item="my data" name="my name" />
 </template>
 
 <script>
+import User from "./User.vue";
+
 export default {
-  components: {},
+  components: {
+    User,
+  },
   name: "Home",
   data() {
-    return {
-      form: {
-        email: "",
-        password: "",
-        country: "",
-        gender: "",
-        tech: [],
-      },
-      errs: [],
-    };
+    return {};
   },
   computed: {},
-  methods: {
-    login(e) {
-      e.preventDefault();
-      console.warn(this.form);
-      this.errs = [];
-      for (const item in this.form) {
-        if (this.form.item === "" || this.form[item].length === 0) {
-          this.errs.push(item);
-        }
-      }
-      if (this.errs.length === 0) {
-        alert("data was submitted as expected");
-      } else {
-        console.warn(this.errs);
-      }
-    },
-  },
+  methods: {},
 };
 </script>
 
---
+++ src/components/User.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- values of item & name still pass through div tag even no settings in Props -->
+  <div>
+    <!-- Non-Props data -->
+    <h1 v-bind="$attrs">user component</h1>
+    <h3>Bruce</h3>
+    <h3>Peter</h3>
+    <h3>Tom</h3>
+    <!-- Props data -->
+    <!-- <h3>{{ name }}, {{ item }}</h3>  -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  //   Props data
+  props: {
+    /* name: String,
+    item: String, */
+  },
+};
+</script>
