4f49a56953d0dfaa209d302fdcdb4f9b7388c90e
YL <simon-ling@hotmail.com>
2021-07-25 21:20:28 -0400

    23-0000 is done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,7 @@
   <h1>simple form</h1>
   {{ form }}
   <form @submit="login">
+    <!-- input field -->
     <label>email</label>
     <input type="email" placeholder="email" v-model="form.email" /><br /><br />
     <label>password</label>
@@ -10,6 +11,39 @@
       placeholder="password"
       v-model="form.password"
     /><br /><br />
+
+    <!-- select box -->
+    <h1>country</h1>
+    <select v-model="form.country">
+      <option value="india">india</option>
+      <option value="usa">usa</option>
+    </select>
+    <br /><br />
+
+    <!-- radio box -->
+    <h1>gender</h1>
+    <label>male</label>
+    <input
+      type="radio"
+      name="gender"
+      value="male"
+      v-model="form.gender"
+    /><br /><br />
+    <label>female</label>
+    <input
+      type="radio"
+      name="gender"
+      value="female"
+      v-model="form.gender"
+    /><br /><br />
+
+    <!-- checkbox -->
+    <h1>tech</h1>
+    <label>java</label>
+    <input type="checkbox" value="java" v-model="form.tech" /><br /><br />
+    <label>python</label>
+    <input type="checkbox" value="python" v-model="form.tech" /><br /><br />
+
     <button type="submit">submit</button>
   </form>
 </template>
@@ -23,6 +57,9 @@ export default {
       form: {
         email: "",
         password: "",
+        country: "",
+        gender: "",
+        tech: [],
       },
     };
   },
@@ -30,7 +67,8 @@ export default {
   methods: {
     login(e) {
       e.preventDefault();
-      console.warn(`login info: ${this.form.email}, ${this.form.password}`);
+      console.warn(this.form);
+      // console.warn(`login info: ${this.form.email}, ${this.form.password}`);
     },
   },
 };
