f94da6ca6a8ebae533bcb0721421f351238ffbe4
YL <simon-ling@hotmail.com>
2021-07-26 18:33:07 -0400

    37-0000 is done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,28 +1,24 @@
 <template>
-  <h1>beforeUpdated & Updated LCMs</h1>
-  <h3 ref="count">{{ counter }}</h3>
-  <button @click="counter = counter + 1">+</button>
+  <h1>beforeUnmounted & Unmounted LCMs</h1>
+  <User v-if="show" />
+  <button @click="show = !show">toggle user</button>
 </template>
 
 <script>
+import User from "./User.vue";
+
 export default {
-  components: {},
+  components: {
+    User,
+  },
   name: "Home",
   data() {
     return {
-      counter: 0,
+      show: true,
     };
   },
   computed: {},
   methods: {},
-  beforeUpdate() {
-    console.warn("beforeUpdate", this.counter); // 1
-    console.warn("beforeUpdate", this.$refs["count"].textContent); // 0
-  },
-  updated() {
-    console.warn("updated", this.counter); //1
-    console.warn("updated", this.$refs["count"].textContent); //1
-  },
 };
 </script>
 
---
+++ src/components/User.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 ref="unmount">user component</h1>
+</template>
+
+<script>
+export default {
+  name: "User",
+  beforeUnmount() {
+    console.warn("beforeUnmount", this.$refs["unmount"]); // beforeUnmount <h1...>user component</h1>
+  },
+  unmounted() {
+    console.warn("unmounted", this.$refs["unmount"]); // unmounted null
+  },
+};
+</script>
