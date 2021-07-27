431ad730fc45751ec85d99ddb35bf340fc4c0f1e
YL <simon-ling@hotmail.com>
2021-07-26 20:38:11 -0400

    42-0000 is done

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div>
     <h1>App component</h1>
-    <router-link to="/">home page</router-link><br /><br />
-    <router-link to="/login">login page</router-link><br /><br />
-    <router-link to="/profile/peter">peter's profile page</router-link
-    ><br /><br />
-    <router-link to="/profile/bruce">bruce's profile page</router-link
-    ><br /><br />
-    <router-link to="/profile/anil">anil's profile page</router-link
-    ><br /><br />
-
+    <p v-for="item in list" :key="item.id">
+      {{ item.email }}, {{ item.first_name }}
+    </p>
     <router-view />
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      list: [],
+    };
+  },
+  async mounted() {
+    const res = await axios.get("https://reqres.in/api/users?page=1");
+    console.warn(res.data.data);
+    this.list = res.data.data;
+  },
 };
 </script>
 
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
-import Home from "./components/Home.vue";
-import Profile from "./components/Profile.vue";
-import Login from "./components/Login.vue";
-import PageNotFound from "./components/PageNotFound.vue";
 
-const routes = [
-  { path: "/", component: Home, name: "Home" },
-  { path: "/login", component: Login, name: "Login" },
-  { path: "/profile/:name", component: Profile, name: "Profile" },
-  { path: "/:pathMath(.*)*", component: PageNotFound, name: "PageNotFound" },
-];
+const routes = [];
 const router = createRouter({
   history: createWebHistory(),
   routes,
